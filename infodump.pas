978719d3b7328f0cbfdadee35ddb06bf943eb79e
Francisco Muñoz <fmunoz.geo@yahoo.com>
2016-05-30 23:46:35 +0200

    Added ranged and melee weapons

--- infodump.pas
+++ infodump.pas
@@ -5,7 +5,7 @@ Program infodump;
 	
 uses Sysutils,gears,arenahq,ability,action,arenacfe,arenascript,backpack,damage,gearutil,ghchars,ghholder,
      ghmodule,ghparser,ghprop,ghswag,interact,menugear,randchar,rpgdice,skilluse,texutil,navigate,
-     congfx,coninfo,conmap,conmenus,context,ghweapon,ghintrinsic;
+     coninfo,conmap,conmenus,context,ghweapon;
 const
 	NumForm = 9;		{ The number of different FORMs which exist in the game.}
 
@@ -28,10 +28,13 @@ var
 	MekList: GearPtr;
 	ShopList: SAttPtr;
 	F : TextFile;
+	F2 :Text;
 begin
 	ShopList := CreateFileList( Design_Directory + Default_Search_Pattern );
 	Assign (F,'dump.txt');
 	rewrite(F);
+	system.WriteLn(F, '==== MECHAS ====');
+
 	system.WriteLn(F, '{| class="article-table sortable"');
 	system.WriteLn(F, '!data-sort-type="number"|Scale');
 	system.WriteLn(F, '!Name');
@@ -68,5 +71,318 @@ begin
 	
 	{ Get rid of the shopping list. }
 	DisposeSAtt( ShopList );
+	DisposeGear( MekList );
+
+	system.WriteLn(F, '==== GUN - Small Arms ====');
+	system.WriteLn(F, '{| class="article-table sortable"');
+	system.WriteLn(F, '!data-sort-type="number"|Scale');
+	system.WriteLn(F, '!Name');
+	system.WriteLn(F, '!data-sort-type="number"|DC');
+	system.WriteLn(F, '!data-sort-type="number"|Range');
+	system.WriteLn(F, '!data-sort-type="number"|ACC');
+	system.WriteLn(F, '!data-sort-type="number"|SPD');
+	system.WriteLn(F, '!data-sort-type="number"|BV');
+	system.WriteLn(F, '!data-sort-type="number"|MAG');
+	system.WriteLn(F, '!data-sort-type="number"|kg');
+	system.WriteLn(F, '!data-sort-type="number"|DP');
+	system.WriteLn(F, '!Effects');
+	system.WriteLn(F, '!data-sort-type="number"|PV');
+
+	//Guns;
+	Assign( F2 , PC_Equipment_File );
+	Reset( F2 );
+	MekList := ReadGear( F2 );
+	Close( F2 );
+	
+	while ( MekList <> Nil ) 
+	do begin
+		if ( ( MekList^.G = GG_Weapon ) and ( MekList^.S = GS_Ballistic) and (  MekList^.V < 11) ) then
+		begin
+			system.WriteLn(F, '|-');
+			system.WriteLn(F, '|' + BStr( MekList^.Scale ));
+			system.WriteLn(F, '|' + FullGearName( MekList ));
+			system.WriteLn(F, '|<nowiki>' + BStr( MekList^.V ) + '</nowiki>');
+			system.WriteLn(F, '|<nowiki>' + BStr( MekList^.Stat[ STAT_Range ] ) + '</nowiki>');
+			system.WriteLn(F, '|<nowiki>' + SgnStr( MekList^.Stat[ STAT_Accuracy ] ) + '</nowiki>');
+			system.WriteLn(F, '|<nowiki>' + BStr( MekList^.Stat[ STAT_Recharge ] ) + '</nowiki>');
+			system.WriteLn(F, '|<nowiki>' + BStr( MekList^.Stat[ STAT_BurstValue ] + 1 ) + '</nowiki>');
+			system.WriteLn(F, '|' + BStr( AmmoRemaining( MekList ) ) );
+			system.WriteLn(F, '|' + MassString( MekList ) );
+			system.WriteLn(F, '|' + BStr( MekList^.V * 3) );
+			system.WriteLn(F, '|' + WeaponAttackAttributes( MekList ) );
+			system.WriteLn(F, '|' + BStr( GearValue( MekList )));
+		end;
+		MekList := MekList^.Next;
+	end;
+		
+	system.WriteLn(F, '|}');
+
+	DisposeGear( MekList );
+
+	system.WriteLn(F, '==== GUN - Heavy Weapons ====');
+	system.WriteLn(F, '{| class="article-table sortable"');
+	system.WriteLn(F, '!data-sort-type="number"|Scale');
+	system.WriteLn(F, '!Name');
+	system.WriteLn(F, '!data-sort-type="number"|DC');
+	system.WriteLn(F, '!data-sort-type="number"|Range');
+	system.WriteLn(F, '!data-sort-type="number"|ACC');
+	system.WriteLn(F, '!data-sort-type="number"|SPD');
+	system.WriteLn(F, '!data-sort-type="number"|BV');
+	system.WriteLn(F, '!data-sort-type="number"|MAG');
+	system.WriteLn(F, '!data-sort-type="number"|kg');
+	system.WriteLn(F, '!data-sort-type="number"|DP');
+	system.WriteLn(F, '!Effects');
+	system.WriteLn(F, '!data-sort-type="number"|PV');
+
+	//Guns;
+	Assign( F2 , PC_Equipment_File );
+	Reset( F2 );
+	MekList := ReadGear( F2 );
+	Close( F2 );
+	
+	while ( MekList <> Nil )
+	do begin
+		if ( ( MekList^.G = GG_Weapon ) and ( MekList^.S = GS_Ballistic) and (  MekList^.V > 11)) then
+		begin
+			system.WriteLn(F, '|-');
+			system.WriteLn(F, '|' + BStr( MekList^.Scale ));
+			system.WriteLn(F, '|' + FullGearName( MekList ));
+			system.WriteLn(F, '|<nowiki>' + BStr( MekList^.V ) + '</nowiki>');
+			system.WriteLn(F, '|<nowiki>' + BStr( MekList^.Stat[ STAT_Range ] ) + '</nowiki>');
+			system.WriteLn(F, '|<nowiki>' + SgnStr( MekList^.Stat[ STAT_Accuracy ] ) + '</nowiki>');
+			system.WriteLn(F, '|<nowiki>' + BStr( MekList^.Stat[ STAT_Recharge ] ) + '</nowiki>');
+			system.WriteLn(F, '|<nowiki>' + BStr( MekList^.Stat[ STAT_BurstValue ] + 1 ) + '</nowiki>');
+			system.WriteLn(F, '|' + BStr( AmmoRemaining( MekList ) ) );
+			system.WriteLn(F, '|' + MassString( MekList ) );
+			system.WriteLn(F, '|' + BStr( MekList^.V * 3) );
+			system.WriteLn(F, '|' + WeaponAttackAttributes( MekList ) );
+			system.WriteLn(F, '|' + BStr( GearValue( MekList )));
+		end;
+		MekList := MekList^.Next;
+	end;
+		
+	system.WriteLn(F, '|}');
+
+	DisposeGear( MekList );
+	
+	//Beam guns
+	system.WriteLn(F, '==== BeamGun - Small Arms ====');
+	system.WriteLn(F, '{| class="article-table sortable"');
+	system.WriteLn(F, '!data-sort-type="number"|Scale');
+	system.WriteLn(F, '!Name');
+	system.WriteLn(F, '!data-sort-type="number"|DC');
+	system.WriteLn(F, '!data-sort-type="number"|Range');
+	system.WriteLn(F, '!data-sort-type="number"|ACC');
+	system.WriteLn(F, '!data-sort-type="number"|SPD');
+	system.WriteLn(F, '!data-sort-type="number"|BV');
+	system.WriteLn(F, '!data-sort-type="number"|kg');
+	system.WriteLn(F, '!data-sort-type="number"|DP');
+	system.WriteLn(F, '!Effects');
+	system.WriteLn(F, '!data-sort-type="number"|PV');
+
+	//Guns;
+	Assign( F2 , PC_Equipment_File );
+	Reset( F2 );
+	MekList := ReadGear( F2 );
+	Close( F2 );
+	
+	while ( MekList <> Nil )
+	do begin
+		if  (( MekList^.G = GG_Weapon ) and ( MekList^.S = GS_Ballistic) and (  MekList^.V < 11) ) then
+		begin
+			system.WriteLn(F, '|-');
+			system.WriteLn(F, '|' + BStr( MekList^.Scale ));
+			system.WriteLn(F, '|' + FullGearName( MekList ));
+			system.WriteLn(F, '|<nowiki>' + BStr( MekList^.V ) + '</nowiki>');
+			system.WriteLn(F, '|<nowiki>' + BStr( MekList^.Stat[ STAT_Range ] ) + '</nowiki>');
+			system.WriteLn(F, '|<nowiki>' + SgnStr( MekList^.Stat[ STAT_Accuracy ] ) + '</nowiki>');
+			system.WriteLn(F, '|<nowiki>' + BStr( MekList^.Stat[ STAT_Recharge ] ) + '</nowiki>');
+			system.WriteLn(F, '|<nowiki>' + BStr( MekList^.Stat[ STAT_BurstValue ] + 1 ) + '</nowiki>');
+			system.WriteLn(F, '|' + MassString( MekList ) );
+			system.WriteLn(F, '|' + BStr( MekList^.V * 3) );
+			system.WriteLn(F, '|' + WeaponAttackAttributes( MekList ) );
+			system.WriteLn(F, '|' + BStr( GearValue( MekList )));
+		end;
+		MekList := MekList^.Next;
+	end;
+		
+	system.WriteLn(F, '|}');
+	DisposeGear( MekList );
+
+	//Beam guns
+	system.WriteLn(F, '==== BeamGun - Heavy Weapons ====');
+	system.WriteLn(F, '{| class="article-table sortable"');
+	system.WriteLn(F, '!data-sort-type="number"|Scale');
+	system.WriteLn(F, '!Name');
+	system.WriteLn(F, '!data-sort-type="number"|DC');
+	system.WriteLn(F, '!data-sort-type="number"|Range');
+	system.WriteLn(F, '!data-sort-type="number"|ACC');
+	system.WriteLn(F, '!data-sort-type="number"|SPD');
+	system.WriteLn(F, '!data-sort-type="number"|BV');
+	system.WriteLn(F, '!data-sort-type="number"|kg');
+	system.WriteLn(F, '!data-sort-type="number"|DP');
+	system.WriteLn(F, '!Effects');
+	system.WriteLn(F, '!data-sort-type="number"|PV');
+
+	//Guns;
+	Assign( F2 , PC_Equipment_File );
+	Reset( F2 );
+	MekList := ReadGear( F2 );
+	Close( F2 );
+	
+	while ( MekList <> Nil ) 
+	do begin
+		if (( MekList^.G = GG_Weapon ) and ( MekList^.S = GS_Ballistic) and (  MekList^.V > 11) ) then
+		begin
+			system.WriteLn(F, '|-');
+			system.WriteLn(F, '|' + BStr( MekList^.Scale ));
+			system.WriteLn(F, '|' + FullGearName( MekList ));
+			system.WriteLn(F, '|<nowiki>' + BStr( MekList^.V ) + '</nowiki>');
+			system.WriteLn(F, '|<nowiki>' + BStr( MekList^.Stat[ STAT_Range ] ) + '</nowiki>');
+			system.WriteLn(F, '|<nowiki>' + SgnStr( MekList^.Stat[ STAT_Accuracy ] ) + '</nowiki>');
+			system.WriteLn(F, '|<nowiki>' + BStr( MekList^.Stat[ STAT_Recharge ] ) + '</nowiki>');
+			system.WriteLn(F, '|<nowiki>' + BStr( MekList^.Stat[ STAT_BurstValue ] + 1 ) + '</nowiki>');
+			system.WriteLn(F, '|' + MassString( MekList ) );
+			system.WriteLn(F, '|' + BStr( MekList^.V * 3) );
+			system.WriteLn(F, '|' + WeaponAttackAttributes( MekList ) );
+			system.WriteLn(F, '|' + BStr( GearValue( MekList )));
+		end;
+		MekList := MekList^.Next;
+	end;
+		
+	system.WriteLn(F, '|}');
+	DisposeGear( MekList );
+	
+	//Missiles guns
+	system.WriteLn(F, '==== Missile Launchers - Heavy Weapons ====');
+	system.WriteLn(F, '{| class="article-table sortable"');
+	system.WriteLn(F, '!data-sort-type="number"|Scale');
+	system.WriteLn(F, '!Name');
+	system.WriteLn(F, '!data-sort-type="number"|DC');
+	system.WriteLn(F, '!data-sort-type="number"|Range');
+	system.WriteLn(F, '!data-sort-type="number"|ACC');
+	system.WriteLn(F, '!data-sort-type="number"|SPD');
+	system.WriteLn(F, '!data-sort-type="number"|MAG');
+	system.WriteLn(F, '!data-sort-type="number"|kg');
+	system.WriteLn(F, '!data-sort-type="number"|DP');
+	system.WriteLn(F, '!Effects');
+	system.WriteLn(F, '!data-sort-type="number"|PV');
+
+	//Guns;
+	Assign( F2 , PC_Equipment_File );
+	Reset( F2 );
+	MekList := ReadGear( F2 );
+	Close( F2 );
+	
+	while ( MekList <> Nil ) 
+	do begin
+		if ( ( MekList^.G = GG_Weapon ) and ( MekList^.S = GS_Ballistic) and (  MekList^.V < 11) ) then
+		begin
+			system.WriteLn(F, '|-');
+			system.WriteLn(F, '|' + BStr( MekList^.Scale ));
+			system.WriteLn(F, '|' + FullGearName( MekList ));
+			system.WriteLn(F, '|<nowiki>' + BStr( MekList^.V ) + '</nowiki>');
+			system.WriteLn(F, '|<nowiki>' + BStr( MekList^.Stat[ STAT_Range ] ) + '</nowiki>');
+			system.WriteLn(F, '|<nowiki>' + SgnStr( MekList^.Stat[ STAT_Accuracy ] ) + '</nowiki>');
+			system.WriteLn(F, '|<nowiki>' + BStr( MekList^.Stat[ STAT_Recharge ] ) + '</nowiki>');
+			system.WriteLn(F, '|' + BStr( AmmoRemaining( MekList ) ) );
+			system.WriteLn(F, '|' + MassString( MekList ) );
+			system.WriteLn(F, '|' + BStr( MekList^.V * 3) );
+			system.WriteLn(F, '|' + WeaponAttackAttributes( MekList ) );
+			system.WriteLn(F, '|' + BStr( GearValue( MekList )));
+		end;
+		MekList := MekList^.Next;
+	end;
+		
+	system.WriteLn(F, '|}');
+	DisposeGear( MekList );
+	
+	//Melee
+	system.WriteLn(F, '==== Melee - Close Combat ====');
+	system.WriteLn(F, '{| class="article-table sortable"');
+	system.WriteLn(F, '!data-sort-type="number"|Scale');
+	system.WriteLn(F, '!Name');
+	system.WriteLn(F, '!data-sort-type="number"|DC');
+	system.WriteLn(F, '!data-sort-type="number"|Range');
+	system.WriteLn(F, '!data-sort-type="number"|ACC');
+	system.WriteLn(F, '!data-sort-type="number"|SPD');
+	system.WriteLn(F, '!data-sort-type="number"|kg');
+	system.WriteLn(F, '!data-sort-type="number"|DP');
+	system.WriteLn(F, '!Effects');
+	system.WriteLn(F, '!data-sort-type="number"|PV');
+	
+	Assign( F2 , PC_Equipment_File );
+	Reset( F2 );
+	MekList := ReadGear( F2 );
+	Close( F2 );
+	
+	while ( MekList <> Nil ) 
+	do begin
+		if ( ( MekList^.G = GG_Weapon ) and ( MekList^.S = GS_Melee ) ) then
+		begin
+			system.WriteLn(F, '|-');
+			system.WriteLn(F, '|' + BStr( MekList^.Scale ));
+			system.WriteLn(F, '|' + FullGearName( MekList ));
+			system.WriteLn(F, '|<nowiki>' + BStr( MekList^.V ) + '</nowiki>');
+			system.WriteLn(F, '|<nowiki>' + BStr( MekList^.Stat[ STAT_Range ] ) + '</nowiki>');
+			system.WriteLn(F, '|<nowiki>' + SgnStr( MekList^.Stat[ STAT_Accuracy ] ) + '</nowiki>');
+			system.WriteLn(F, '|<nowiki>' + BStr( MekList^.Stat[ STAT_Recharge ] ) + '</nowiki>');
+			system.WriteLn(F, '|' + MassString( MekList ) );
+			system.WriteLn(F, '|' + BStr( MekList^.V * 3) );
+			system.WriteLn(F, '|' + WeaponAttackAttributes( MekList ) );
+			system.WriteLn(F, '|' + BStr( GearValue( MekList )));
+		end;
+		MekList := MekList^.Next;
+	end;
+		
+	system.WriteLn(F, '|}');
+	DisposeGear( MekList );
+	
+	//EMelee
+
+	system.WriteLn(F, '==== Energy Melee - Close Combat ====');
+	system.WriteLn(F, '{| class="article-table sortable"');
+	system.WriteLn(F, '!data-sort-type="number"|Scale');
+	system.WriteLn(F, '!Name');
+	system.WriteLn(F, '!data-sort-type="number"|DC');
+	system.WriteLn(F, '!data-sort-type="number"|Range');
+	system.WriteLn(F, '!data-sort-type="number"|ACC');
+	system.WriteLn(F, '!data-sort-type="number"|SPD');
+	system.WriteLn(F, '!data-sort-type="number"|kg');
+	system.WriteLn(F, '!data-sort-type="number"|DP');
+	system.WriteLn(F, '!Effects');
+	system.WriteLn(F, '!data-sort-type="number"|PV');
+	
+	Assign( F2 , PC_Equipment_File );
+	Reset( F2 );
+	MekList := ReadGear( F2 );
+	Close( F2 );
+	
+	while ( MekList <> Nil ) 
+	do begin
+		if ( ( MekList^.G = GG_Weapon ) and ( MekList^.S = GS_EMelee ) ) then
+		begin
+			system.WriteLn(F, '|-');
+			system.WriteLn(F, '|' + BStr( MekList^.Scale ));
+			system.WriteLn(F, '|' + FullGearName( MekList ));
+			system.WriteLn(F, '|<nowiki>' + BStr( MekList^.V ) + '</nowiki>');
+			system.WriteLn(F, '|<nowiki>' + BStr( MekList^.Stat[ STAT_Range ] ) + '</nowiki>');
+			system.WriteLn(F, '|<nowiki>' + SgnStr( MekList^.Stat[ STAT_Accuracy ] ) + '</nowiki>');
+			system.WriteLn(F, '|<nowiki>' + BStr( MekList^.Stat[ STAT_Recharge ] ) + '</nowiki>');
+			system.WriteLn(F, '|' + MassString( MekList ) );
+			system.WriteLn(F, '|<nowiki>' + BStr( MekList^.V * 3 ) + '</nowiki>');
+			system.WriteLn(F, '|' + WeaponAttackAttributes( MekList ) );
+			system.WriteLn(F, '|' + BStr( GearValue( MekList )));
+		end;
+		MekList := MekList^.Next;
+	end;
+		
+	system.WriteLn(F, '|}');
+	DisposeGear( MekList );
+	
+	
 	System.Close (F);
 end.
+
+	//PC_Equipment_File = Design_Directory + 'PC_Equipment.txt';
+	//Mek_Equipment_File = Design_Directory + 'Mek_Equipment.txt';
